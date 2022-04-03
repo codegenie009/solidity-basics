0b2ef18a0b8af8c9050ff54a746606127f388358
gs <ghostson1120@outlook.com>
2022-04-03 22:56:37 +0800

    voting

--- examples/voting.sol
+++ examples/voting.sol
@@ -22,5 +22,12 @@ contract Ballot {
   constructor(bytes32[] memory proposalNames) {
     chairperson = msg.sender;
     voters[chairperson].weight = 1;
+
+    for (uint i = 0; i < proposalNames.length; i++) {
+      proposals.push(Proposal({
+        name: proposalNames[i],
+        voteCount: 0
+      }));
+    }
   }
 }
